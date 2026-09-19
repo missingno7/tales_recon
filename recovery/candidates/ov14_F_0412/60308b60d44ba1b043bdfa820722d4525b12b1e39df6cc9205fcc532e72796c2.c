a9169b955db25bca6f5fc553a1378c9e19cb1a0a
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 22:47:41 +0200

    Refine ov14 return codegen blocker

---
+++ recovery/candidates/ov14_F_0412/60308b60d44ba1b043bdfa820722d4525b12b1e39df6cc9205fcc532e72796c2.c
@@ -0,0 +1,18 @@
+struct Item { int pad; int value; };
+struct Record { struct Item *item; int pad; char a; char b; char tail[4]; };
+extern struct Record G_h01_4528[36];
+unsigned int recovered(skip)
+char skip;
+{
+ char i, chosen;
+ int best, value;
+ best=0;
+ chosen=35;
+ for(i=1;i<35;i++) {
+  if(i!=skip && !G_h01_4528[i].a && !G_h01_4528[i].b) {
+   value=G_h01_4528[i].item->value;
+   if(value>best) { best=value; chosen=i; }
+  }
+ }
+ return chosen+=0;
+}
