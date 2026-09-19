3e350a9520ab2e9b04565a75f95fe2bb94b62bd0
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 19:13:13 +0200

    Retain ov14 indexed record blocker

---
+++ recovery/candidates/ov14_F_0412/8fa2e1ed62da8c2cc75b48dbd5f6d34759b883b495d20dc1d937a6147f3f6dd8.c
@@ -0,0 +1,29 @@
+struct Item {
+    char pad[2];
+    int value;
+};
+
+extern char G_h01_4528[432];
+extern char G_h01_452E[432];
+extern char G_h01_452F[432];
+
+unsigned char recovered(skip)
+char skip;
+{
+    unsigned char i;
+    char chosen;
+    int best,value;
+
+    best=0;
+    chosen=35;
+    for (i=1;i<35;i++) {
+        if (i!=skip && !G_h01_452E[(unsigned long)i*12] && !G_h01_452F[(unsigned long)i*12]) {
+            value=(*(struct Item **)(G_h01_4528+(unsigned long)i*12))->value;
+            if (value>best) {
+                best=value;
+                chosen=i;
+            }
+        }
+    }
+    return chosen;
+}
---
+++ recovery/candidates/ov14_F_0412/a4b451df36592915c377f0131b6fddc1fb2e1949adc65f264dcbe50a7cad28c8.c
@@ -0,0 +1,28 @@
+struct Item {
+    char pad[2];
+    int value;
+};
+
+extern char G_h01_4528[432];
+extern char G_h01_452E[432];
+extern char G_h01_452F[432];
+
+unsigned char recovered(skip)
+char skip;
+{
+    char i,chosen;
+    int best,value;
+
+    best=0;
+    chosen=35;
+    for (i=1;i<35;i++) {
+        if (i!=skip && !G_h01_452E[i*12] && !G_h01_452F[i*12]) {
+            value=(*(struct Item **)(G_h01_4528+i*12))->value;
+            if (value>best) {
+                best=value;
+                chosen=i;
+            }
+        }
+    }
+    return chosen;
+}
