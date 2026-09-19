eae614a04028c9494b5ab33e647e839c7a5c4e6e
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 17:48:10 +0200

    Recover ov07 parser leaf

---
+++ recovery/candidates/ov07_F_18F8/e0fdd4a242c0d658334e809ab9563c4352894c9e2b45246992105ba12fc28391.c
@@ -0,0 +1,42 @@
+struct Input {
+    char pad[2];
+    long text;
+};
+
+extern struct Input *G_h01_0012;
+extern int F_h00_0976();
+extern int F_h00_35DC();
+
+recovered(unused, mode, x, y)
+int unused;
+int mode;
+int x;
+int y;
+{
+    int i;
+    int limit;
+    long unused_local;
+    char data[60];
+    long text;
+
+    for (i = 0; i < 60; i++)
+        data[i] = 0;
+    text = F_h00_0976(G_h01_0012->text, data);
+    text = F_h00_0976(text, data);
+    if (mode == 1)
+        limit = 6;
+    else if (mode == 2) {
+        for (i = 0; i < 6; i++)
+            text = F_h00_0976(text, data);
+        limit = 2;
+    } else if (mode == 3) {
+        for (i = 0; i < 8; i++)
+            text = F_h00_0976(text, data);
+        limit = 2;
+    }
+    for (i = 0; i < limit; i++) {
+        text = F_h00_0976(text, data);
+        F_h00_35DC(x, y, data, 1, 0);
+        y += 10;
+    }
+}
