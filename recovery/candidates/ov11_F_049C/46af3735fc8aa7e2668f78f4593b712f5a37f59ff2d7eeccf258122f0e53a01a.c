01f7eba790b751a71098ea6e314a1ba679c427d6
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 18:33:44 +0200

    Recover ov11 resident dispatch sequence

---
+++ recovery/candidates/ov11_F_049C/46af3735fc8aa7e2668f78f4593b712f5a37f59ff2d7eeccf258122f0e53a01a.c
@@ -0,0 +1,42 @@
+/* Direct reconstruction candidate for ov11_F_049C. */
+extern long G_h01_9F4E;
+extern long G_h01_9F52;
+extern long G_h01_9F56;
+extern long G_h01_9F5E;
+extern long G_h01_9F62;
+extern long G_h01_9F66;
+extern long G_h01_9F6A;
+extern long G_h01_9F6E;
+extern long G_h01_9F76;
+extern long G_h01_9F7E;
+extern long G_h01_9F82;
+extern long G_h01_9F8A;
+extern long G_h01_9F8E;
+extern long G_h01_9F92;
+extern long G_h01_9F96;
+extern long G_h01_9F9A;
+extern long G_h01_9F9E;
+extern long G_h01_9FA2;
+extern int F_h00_3BD2();
+
+recovered()
+{
+    F_h00_3BD2(G_h01_9F8E);
+    F_h00_3BD2(G_h01_9F4E);
+    F_h00_3BD2(G_h01_9F52);
+    F_h00_3BD2(G_h01_9F56);
+    F_h00_3BD2(G_h01_9F5E);
+    F_h00_3BD2(G_h01_9F62);
+    F_h00_3BD2(G_h01_9F66);
+    F_h00_3BD2(G_h01_9F6A);
+    F_h00_3BD2(G_h01_9F82);
+    F_h00_3BD2(G_h01_9F7E);
+    F_h00_3BD2(G_h01_9F6E);
+    F_h00_3BD2(G_h01_9F76);
+    F_h00_3BD2(G_h01_9F8A);
+    F_h00_3BD2(G_h01_9F92);
+    F_h00_3BD2(G_h01_9F96);
+    F_h00_3BD2(G_h01_9F9A);
+    F_h00_3BD2(G_h01_9F9E);
+    F_h00_3BD2(G_h01_9FA2);
+}
