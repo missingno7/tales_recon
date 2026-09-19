315d17c4a7d2990052ba787f8e0f6008490c1a08
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 18:20:50 +0200

    Recover ov04 fixed status dispatch leaf

---
+++ src/recovered/ov04/ov04_F_0DBE.c
@@ -0,0 +1,19 @@
+/* Direct reconstruction candidate for ov04_F_0DBE. */
+extern int G_h01_51FC; extern int G_h01_51FE; extern char G_h01_5204;
+extern int G_h01_5232; extern int G_h01_5234; extern char G_h01_523A;
+extern int G_h01_529E; extern int G_h01_52A0; extern char G_h01_52A6;
+extern int G_h01_52D4; extern int G_h01_52D6; extern char G_h01_52DC;
+extern int G_h01_5268; extern int G_h01_526A; extern char G_h01_5270;
+extern int G_h01_51C6; extern int G_h01_51C8; extern char G_h01_51CE;
+extern int G_h01_530A; extern int G_h01_530C; extern char G_h01_5312;
+extern int F_h00_3AE4();
+recovered()
+{
+ F_h00_3AE4(&G_h01_5204,G_h01_51FC,G_h01_51FE);
+ F_h00_3AE4(&G_h01_523A,G_h01_5232,G_h01_5234);
+ F_h00_3AE4(&G_h01_52A6,G_h01_529E,G_h01_52A0);
+ F_h00_3AE4(&G_h01_52DC,G_h01_52D4,G_h01_52D6);
+ F_h00_3AE4(&G_h01_5270,G_h01_5268,G_h01_526A);
+ F_h00_3AE4(&G_h01_51CE,G_h01_51C6,G_h01_51C8);
+ F_h00_3AE4(&G_h01_5312,G_h01_530A,G_h01_530C);
+}
