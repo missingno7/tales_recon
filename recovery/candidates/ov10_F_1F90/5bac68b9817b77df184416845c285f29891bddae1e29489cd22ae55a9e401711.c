cde9daa7e0087f6800c81cc381eeb29970d601a7
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 19:09:08 +0200

    Recover ov10 threshold dispatcher

---
+++ recovery/candidates/ov10_F_1F90/5bac68b9817b77df184416845c285f29891bddae1e29489cd22ae55a9e401711.c
@@ -0,0 +1,14 @@
+extern F_h00_57D2();
+
+recovered(a)
+int a;
+{
+    if (a < 2)
+        F_h00_57D2(0x51);
+    else if (a < 4)
+        F_h00_57D2(0x50);
+    else if (a < 6)
+        F_h00_57D2(0x4f);
+    else
+        F_h00_57D2(0x4e);
+}
